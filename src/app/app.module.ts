8988b51b562a13a6759c44dbcbe641ecd0bab778
Agata <blumf@poczta.onet.pl>
2018-09-02 22:16:24 +0200

    initial
    initila

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './view/login-component/login-component.component';
+import { DashboardComponentComponent } from './view/dashboard-component/dashboard-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    DashboardComponentComponent
   ],
   imports: [
     BrowserModule
