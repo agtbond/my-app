54926ff43945b9553f6e9760c19231fa2134d42b
Agata <blumf@poczta.onet.pl>
2018-09-11 15:25:44 +0200

    http

--- src/app/services/lyrics.service.ts
+++ src/app/services/lyrics.service.ts
@@ -1,17 +1,33 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Songs } from '../models/songs';
-import { Observable } from '../../../node_modules/rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+// export class LyricsService {
+
+//   constructor(private http: HttpClient) {
+//   }
+
+//   fetchLyrics(songChoosen: Songs) {
+//     return this.http.get(`https://api.lyrics.ovh/v1/${songChoosen.band}/${songChoosen.song}`);
+//   }
+// }
+
 export class LyricsService {
 
   constructor(private http: HttpClient) {
   }
 
-  fetchLyrics(songChoosen: Songs): Observable<Songs> {
-    return this.http.get<Songs>(`https://api.lyrics.ovh/v1/${songChoosen.band}/${songChoosen.song}`);
+  fetchLyrics(dto: FetchLyricsDto) {
+    return this.http.get(`https://api.lyrics.ovh/v1/${dto.band}/${dto.song}`);
   }
 }
+
+
+interface FetchLyricsDto {
+  band: string;
+  song: string;
+}
+
